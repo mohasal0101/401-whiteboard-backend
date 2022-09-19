e89cc0cbcdbab0653ee9acaf2b65b14804c72e73
mohasal0101 <mohasal0101@gmail.com>
2022-09-19 15:45:15 +0300

    add-user-auth

--- models/index.js
+++ models/index.js
@@ -5,6 +5,8 @@ const post = require( './post.model' );
 const comment = require('./comment.model')
 const POSTGRES_URL = process.env.DATABASE_URL || "postgres://vvhjywzvxbcqqt:e41b904e5cca6e8e0873ee39f828222669474e387fe5b4fa4d5c37b4c82fd850@ec2-54-246-185-161.eu-west-1.compute.amazonaws.com:5432/dc1q397lnip5a";
 const collection = require('../collections/user-comment-routes')
+const user = require('./user.model');
+
 
 const sequelizeOption = {
     dialectOptions: {
@@ -18,6 +20,8 @@ const sequelizeOption = {
 let sequelize = new Sequelize( POSTGRES_URL, sequelizeOption );
 const postModel = post(sequelize, DataTypes);
 const commentModel = comment(sequelize,DataTypes);
+const userModel = user(sequelize,DataTypes);
+
 
 postModel.hasMany(commentModel, {foreignKey: 'ownerID', sourceKey: 'id'})
 commentModel.belongsTo(postModel, {foreignKey: 'ownerID', targetKey: 'id'})
@@ -31,5 +35,6 @@ module.exports = {
     db: sequelize,
     Post: postCollection,
     Comment: commentCollection,
-    CommentModel: commentModel
+    CommentModel: commentModel,
+    UserModel: userModel
 };
