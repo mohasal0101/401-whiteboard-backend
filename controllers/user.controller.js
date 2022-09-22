4856d021a7556ab744f1ef7f4a2f499e673dc239
mohasal0101 <mohasal0101@gmail.com>
2022-09-22 18:51:04 +0300

    debugged controller

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -1,45 +1,53 @@
 'use strict';
 const bcrypt = require( 'bcrypt' );
 const base64 = require( 'base-64' );
-const { UserModel } = require( '../models/index' );
+
+const User = require( '../models' ).UserModel;
 
 const signup = async ( req, res ) => {
     try {
-        req.body.password = await bcrypt.hash( req.body.password, 10 );
-        const user = await UserModel.create( req.body );
-        const output = {
-            user: user,
-            token: user.token
+        const { username, email, password } = req.body;
+        const data = {
+            username,
+            email,
+            password: await bcrypt.hash( password, 15 )
         };
-        res.status( 201 ).json( output );
+        const user = await User.create( data );
+        if ( user ) {
+            res.status( 200 ).json( user );
+        }
     } catch ( error ) {
-        res.status( 403 ).send( 'Error Creating User' );
+        console.log( error );
     }
 };
 
 const allUser = async ( req, res ) => {
-    const users = await UserModel.findAll();
-    res.status( 200 ).json( users );
+    const users = await User.findAll();
+    res.json( users );
 };
 
 const login = async ( req, res ) => {
-    try {
-        const user = await UserModel.findOne( { where: { username: req.body.username } } );
-        const valid = await bcrypt.compare( req.body.password, user.password );
-        if ( valid ) {
-            const output = {
-                user: user,
-                token: user.token
-            };
-            res.status( 200 ).json( output );
-        } else {
-            throw new Error( 'Invalid Login' );
+    const basicHeader = req.headers.authorization.split( ' ' );
+    const encodedValue = basicHeader.pop();
+    const decodedValue = base64.decode( encodedValue );
+    console.log( decodedValue );
+    const [ username, password ] = decodedValue.split( ':' );
+    const user = await User.findOne( {
+        where: {
+            username: username
         }
-    } catch ( error ) {
-        res.status( 403 ).send( 'Invalid Login' );
+    } );
+    if ( user ) {
+        const isSame = await bcrypt.compare( password, user.password );
+        if ( isSame ) {
+            return res.status( 200 ).json( user );
+        } else {
+            return res.status( 401 ).send( 'You are not authorized' );
+        }
+    } else {
+        return res.status( 401 ).send( 'You are not authorized' );
     }
 };
-
 
 
 module.exports = {
